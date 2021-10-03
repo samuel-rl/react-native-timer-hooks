9e8d9f2bee9ab9bbd67540ac42fe94d58c9a0205
Samuel <samuel310597@gmail.com>
2021-10-03 19:04:09 +0200

    feat: create useClock

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,32 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
+import { useEffect, useRef, useState } from 'react';
+
+export const useClock: (
+  total: number,
+  ms?: number,
+  down?: boolean
+) => [number, () => void, () => void, (from?: number) => void, boolean] = (
+  total: number,
+  ms: number = 1000,
+  down: boolean = false
+) => {
+  const [counter, setCounter] = useState(total);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const intervalId = useRef<any>();
+  const start: () => void = () => setIsRunning(true);
+  const pause: () => void = () => setIsRunning(false);
+  const reset: (from?: number) => void = (from?: number) => {
+    clearInterval(intervalId.current);
+    setCounter(from ? from : total);
+  };
+
+  useEffect(() => {
+    intervalId.current = setInterval(() => {
+      isRunning && setCounter(down ? counter - 1 : counter + 1);
+    }, ms);
+    // Clear interval when unmount
+    return () => clearInterval(intervalId.current);
+  }, [isRunning, counter, ms, down]);
+
+  return [counter, start, pause, reset, isRunning];
 };
